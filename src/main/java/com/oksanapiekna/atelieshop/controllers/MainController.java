c2639c60ae96e84dbeff873609109eef33a7cc0f
roman <romantymochko@gmail.com>
2020-11-08 00:47:57 +0100

    resources locations

--- src/main/java/com/oksanapiekna/atelieshop/controllers/MainController.java
+++ src/main/java/com/oksanapiekna/atelieshop/controllers/MainController.java
@@ -1,8 +1,15 @@
 package com.oksanapiekna.atelieshop.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
+    @GetMapping ("/")
+    public String getIndex(Model model){
+        return "index_new";
+    }
 
 }
